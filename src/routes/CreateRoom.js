f04389c409c5af2b8b885fea68fd1f351cabb8d9
BALAJI934428 <142514847+BALAJI934428@users.noreply.github.com>
2024-02-05 04:08:03 +0530

    Update CreateRoom.js

--- src/routes/CreateRoom.js
+++ src/routes/CreateRoom.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const CreateRoomController = require("../controller/CreateRoom");
+const CreateRoomController = require("../controller/Createroom");
 router.post("/", CreateRoomController.createRoom);
 module.exports = router;
